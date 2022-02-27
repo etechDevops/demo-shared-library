d45a905876cc9330309a9e8666852afd995596bd
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2022-02-27 17:38:49 -0500

    Create springApp.groovy

---
+++ vars/springApp.groovy
@@ -0,0 +1,36 @@
+pipeline {
+       agent any
+       tools {
+           maven 'Maven 3.6.0'
+           jdk 'jdk11'
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                   sh "mvn --version"
+                   sh "java -version"
+               }
+           }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                       url: "https://github.com/iamvickyav/spring-boot-data-H2-embedded.git"
+               }
+           }
+           stage("Cleaning workspace") {
+               steps {
+                   sh "mvn clean"
+               }
+           }
+           stage("Running Testcase") {
+              steps {
+                   sh "mvn test"
+               }
+           }
+           stage("Packing Application") {
+               steps {
+                   sh "mvn package -DskipTests"
+               }
+           }
+       }
+ }
